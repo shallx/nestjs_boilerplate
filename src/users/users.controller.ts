2a4d6a560f4337945d30a4410f64c5c868c4cae4
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2022-09-07 17:36:58 +0600

    params and query params implemented

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,43 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
+import { CreateUserDto } from './dtos/CreateUser.dto';
 
 @Controller('users')
 export class UsersController {
   @Get()
-  getUsers() {
+  getUsers(@Query('sortBy') sortBy: string) {
+    console.log(sortBy);
     return { user: 'Rafat Rashid Rahi' };
   }
 
+  // @Post()
+  // createUsers(@Req() request: Request, @Res() response: Response) {
+  //   console.log(request.body);
+  //   response.json({ name: 'some name' });
+  // }
+
   @Post()
-  createUsers(@Req() request: Request, @Res() response: Response) {
-    console.log(request.body);
-    response.json({ name: 'some name' });
+  createUsers(@Body() userData: CreateUserDto) {
+    console.log(userData);
+    return {};
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Get(':id/:postId')
+  getPostById(@Param('id') id: string, @Param('postId') postId: string) {
+    return { id, postId };
   }
 }
