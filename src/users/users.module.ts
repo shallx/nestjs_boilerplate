f92a213a6a1b34288a86e4724da1e2ef3bcb0909
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2022-09-16 02:39:20 +0600

    Middleware done

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,6 +2,7 @@ import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/c
 import { UsersController } from './users.controller';
 import { UsersService } from './services/users/users.service';
 import { UsersMiddleware } from './middleware/users.middleware';
+import { AnotherMiddleware } from './middleware/another.middleware';
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
@@ -9,10 +10,13 @@ import { UsersMiddleware } from './middleware/users.middleware';
 // This setup is required for Setting up Middleware consumer
 export class UsersModule implements NestModule{
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(UsersMiddleware).forRoutes(UsersController); // .forRoutes('users') can also be used
-    // consumer.apply(UsersMiddleware).forRoutes({
-    //   path: 'users',
-    //   method: RequestMethod.GET,
-    // })
+    consumer.apply(UsersMiddleware)
+      .forRoutes(UsersController) // .forRoutes('users') can also be used
+      .apply(AnotherMiddleware).forRoutes({
+        path: 'users/:id',
+        method: RequestMethod.GET,
+      })
+    // Middleware can also be chained to same route
+    // Middleware can also be applied to multiple routes
   }
 }
