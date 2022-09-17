1ba72ea8835804eaeced2a55bfedb13041e279d2
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2022-09-17 10:31:49 +0600

    typeorm added, usermodel added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // TypeOrmModule maynot always auto import
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'testuser',
+      password: '',
+      database: 'node_nestjs_crash2',
+      entities: [],
+      synchronize: true, // synchronize will create table automatically if needed
+    }),
+  ], // This maynot Auto import always
   controllers: [AppController],
   providers: [AppService],
 })
