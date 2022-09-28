9766b971c491e1da1e742e43e660cbeb9205b5b4
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2022-09-28 21:46:43 +0600

    Docker setup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
-@Controller()
+@Controller('auth')
 export class AuthController {
-    
+    constructor(private authService: AuthService){}
+
+    @Post('signup')
+    signup(){
+        return "I just signed up";
+    }
+
+    @Post('signin')
+    signin(){
+        return "I just signed in";
+    }
 }
