55e33d5e6b31d1e404b4fffe5d9900f3789adf8d
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2022-09-28 20:22:39 +0600

    Auth Service, Auth Controller added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 @Module({
   controllers: [],
   providers: [],
+  imports: [AuthModule, UserModule]
 })
 export class AppModule {}
