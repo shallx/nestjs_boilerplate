e89f476d8a380fdadae5a267a68662b7a3bb3cce
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2022-10-09 20:09:20 +0600

    config added, linked to env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
+import { Module } from "@nestjs/common"
+import { AuthModule } from "./auth/auth.module"
+import { UserModule } from "./user/user.module"
+import { PrismaModule } from "./prisma/prisma.module"
+import { BookmarkModule } from "./bookmark/bookmark.module"
+import { ConfigModule } from "@nestjs/config"
 @Module({
   controllers: [],
   providers: [],
-  imports: [AuthModule, UserModule, PrismaModule, BookmarkModule,]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true // Makes it available to other modules
+    }),
+    AuthModule,
+    UserModule,
+    PrismaModule,
+    BookmarkModule,
+  ],
 })
 export class AppModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor(){
+    constructor(config: ConfigService){ // As ConfigModule is imported at appModule, it gets available as ConfigService here
         super({
             datasources: {
                 db: {
-                    url: 'postgresql://postgres:123456@localhost:5434/nest_learn?schema=public'
+                    url: config.get('DATABASE_URL') // 'DATABASE_URL' corresponds to env file
                 }
             }
         })
