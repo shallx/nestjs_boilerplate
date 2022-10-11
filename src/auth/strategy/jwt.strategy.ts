78b441e838c3bd5507b14fcbbad2f53d7c076547
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2022-10-11 22:11:56 +0600

    Auth Guard added to users/me

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -3,13 +3,22 @@ import { ConfigService } from "@nestjs/config"
 import { PassportStrategy } from "@nestjs/passport"
 import { ExtractJwt, Strategy } from "passport-jwt"
 
-@Injectable()// if we don't use injector, we cant get params in constructor
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(config: ConfigService) { // Whatever is passed to authModule is also available here
+@Injectable() // if we don't use injector, we cant get params in constructor
+export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
+  constructor(config: ConfigService) {
+    // Whatever is passed to authModule is also available here
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: config.get('JWT_SECRET'),
+      secretOrKey: config.get("JWT_SECRET"),
     })
+
+    
+  }
+
+  // Converts the token into object
+  validate(payload: any) {
+
+    return payload;
   }
 }
