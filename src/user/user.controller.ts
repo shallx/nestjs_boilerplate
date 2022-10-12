8c3ce815820de87bbec0fdf4e9f24b8671462948
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2022-10-12 22:35:49 +0600

    Created JwtGuard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users') // this creates a route /users
 export class UserController {
-    @UseGuards(AuthGuard('jwt'))
+    @UseGuards(JwtGuard)
     @Get('me')
     getMe(@Req() req: Request){
-        return req.user;
+        return req.user; // req.user comes from jwt.strategy.ts, whatever is passed by validate method
     }
 }
