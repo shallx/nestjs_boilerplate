a1d2c7d4a88b428a27b0c68bfa339b3834713eab
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2022-10-20 22:46:59 +0600

    Functions in bookmark service created

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,20 +1,43 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
-import { GetUser } from 'src/auth/decorator';
-import { JwtGuard } from 'src/auth/guard';
-import { CreateBookmarkDto } from './dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from "@nestjs/common"
+import { GetUser } from "src/auth/decorator"
+import { JwtGuard } from "src/auth/guard"
+import { CreateBookmarkDto } from "./dto"
+import { EditBookmarkDto } from "./dto/edit-bookmark.dto"
 
-@Controller('bookmarks')
+@Controller("bookmarks")
 @UseGuards(JwtGuard)
 export class BookmarkController {
-    @Get()
-    getBookmarks(@GetUser('id') id: number){}
+  @Get()
+  getBookmarks(@GetUser("id") id: number) {}
 
-    @Post()
-    createBookmark(@GetUser('id') id: number, @Body() dto: CreateBookmarkDto){}
+  @Post()
+  createBookmark(@GetUser("id") id: number, @Body() dto: CreateBookmarkDto) {}
 
-    @Get(':id')
-    getBookmarkById(@GetUser('id') id: number, @Param('id', ParseIntPipe) bookmarkId : number){}
+  @Get(":id")
+  getBookmarkById(
+    @GetUser("id") id: number,
+    @Param("id", ParseIntPipe) bookmarkId: number,
+  ) {}
 
-    @Delete(':id')
-    deleteBookmarkById(@GetUser('id') id: number){}
+  @Patch()
+  editBookmarkById(
+    @GetUser("id") id: number,
+    @Param("id", ParseIntPipe) bookmarkId: number,
+    @Body() dto: EditBookmarkDto,
+  ) {}
+  @Delete(":id")
+  deleteBookmarkById(
+    @GetUser("id") id: number,
+    @Param("id", ParseIntPipe) bookmarkId: number,
+  ) {}
 }
--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
 
 @Injectable()
-export class BookmarkService {}
+export class BookmarkService {
+    getBookmarks(userId: number){}
+
+    createBookmark(userId: number, dto: CreateBookmarkDto){}
+
+    getBookmarkById(userId: number, bookmarkId : number){}
+
+    editBookmarkById(userId: number, bookmarkId : number, dto: EditBookmarkDto){}
+
+    deleteBookmarkById(userId: number, bookmarkId : number){}
+}
