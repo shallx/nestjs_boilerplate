cec95f145400baa45fdc683e72cf74f89b45ceab
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2023-05-27 18:27:11 +0600

    Working on relational filter

--- prisma/scripts.ts
+++ prisma/scripts.ts
@@ -7,7 +7,12 @@ async function main() {
   // UserService.findByCompositeKey();
   // findAllUser();
   // UserService.createManyUser()
-  UserService.getByPagination()
+  // UserService.getByPagination()
+  // UserService.getUsersByFilter();
+
+  PostService.createPost()
+  // const post = await  prisma.post.findMany();
+  // console.log(post);
 }
 
 class UserService {
@@ -83,16 +88,116 @@ class UserService {
     console.log(users);
   };
 
-  static getByPagination = async() => {
+  static getByPagination = async () => {
     const users = await prisma.user.findMany({
       orderBy: {
-        age: 'asc',
+        age: "asc",
       },
       take: 2,
       skip: 1,
     });
     console.log(users);
   };
+
+  // Filtering clauses
+  static getUsersByFilter = async () => {
+    // Returns email that do not match with provided email
+    const email1 = {
+      not: "rahi@g.com",
+    };
+
+    // matches with both email
+    const email2 = {
+      in: ["rahi@g.com", "nazia@g.com"],
+    };
+
+    // Provides all but the provided emails in notIn
+    const email3 = {
+      notIn: ["rahi@g.com", "nazia@g.com"],
+    };
+
+    // contains is similar to 'like' operator matches  %@g.com%
+    const email4 = {
+      contains: "@g.com",
+    };
+
+    const email5 = {
+      startsWith: "ra",
+    };
+
+    const email6 = {
+      endsWith: "ra",
+    };
+
+    // complex AND query
+    const where1 = {
+      AND: [
+        // OR, NOT can also be used
+        { email: { startsWith: "r" } },
+        { email: { endsWith: ".com" } },
+      ],
+    };
+
+    // less then spacified age
+    const age = {
+      lt: 26, // other options = [gt : greater than, lte : less then or equal, gte: greather than or equal]
+    };
+    const users = await prisma.user.findMany({
+      where: where1,
+    });
+
+    console.log(users);
+  };
+
+  static relationalFiltering = async () => {
+    // gets user how has email preference and with emailUpdates
+    const user = await prisma.user.findMany({
+      where: {
+        userPreference: {
+          emailUpdates: true,
+        },
+      },
+    });
+
+    //
+    const user2 = await prisma.user.findMany({
+      where: {
+        writtenPosts: {
+          every: {
+            title: "Test",
+          },
+        },
+      },
+    });
+  };
+}
+
+class PostService {
+  static createPost = async() => {
+    const user = await prisma.user.findFirst({
+      where: {
+        name: "Rahi"
+      }
+    })
+
+    if(user){
+      const post = await prisma.post.create({
+        data: {
+          authorId: user.id,
+          title: 'Some title',
+          averageRating: 2.5
+        }
+      })
+      console.log(post)
+
+    }
+
+    
+
+    console.log(user)
+    
+  }
+
 }
 
 main()
